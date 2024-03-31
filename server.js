7a5107b619c834e95138ba1942120b2e55f375bb
Harshk9599 <150929870+Harshk9599@users.noreply.github.com>
2024-03-31 23:37:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 })
 
 // app.listen(port, () => {
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
